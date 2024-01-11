264c5d6680d0f317b8bab6dba35045aac3af1a38
Daniil Cherepkov <132902559+daniilcdev@users.noreply.github.com>
2024-01-11 12:59:15 +0300

    Update filters.sql

--- queries/filters.sql
+++ queries/filters.sql
@@ -3,7 +3,7 @@ SELECT name FROM filters;
 
 -- name: GetReceiptOrDefault :one
 SELECT id, name, receipt FROM filters
-WHERE name = ? OR id = 1;
+WHERE name = ?;
 
 -- name: CreateReceipt :exec
 INSERT INTO filters (name, receipt)
