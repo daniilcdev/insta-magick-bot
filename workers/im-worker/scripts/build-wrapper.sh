17859743b9cae98cb641686e033ca848acd65059
Daniil Cherepkov <groznybear@gmail.com>
2024-02-02 22:37:04 +0300

    fix(im-worker): go build script

--- workers/im-worker/scripts/build-wrapper.sh
+++ workers/im-worker/scripts/build-wrapper.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-go version
-echo $DOCKER_LOCAL_MOUNT
-go build -o=$DOCKER_LOCAL_MOUNT/im-worker -v ./workers/im-worker/cmd/im-service/
-cp -r ./workers/im-worker/config/env $DOCKER_LOCAL_MOUNT/config
+
+go build -o=$DOCKER_LOCAL_MOUNT/im-worker -v ./cmd/im-service/
+cp -r ./config/env $DOCKER_LOCAL_MOUNT/config
 
 sudo docker compose up -d im-worker --build
