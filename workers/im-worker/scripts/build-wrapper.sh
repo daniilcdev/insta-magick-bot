221d2b1cf308cf5627416825ab325ff82b0e1d27
Daniil Cherepkov <groznybear@gmail.com>
2024-02-02 23:57:53 +0300

    fix(im-worker): brute force build #2

--- workers/im-worker/scripts/build-wrapper.sh
+++ workers/im-worker/scripts/build-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 go build -o=/mnt/storage/im-worker-volume/im-worker -v ./cmd/im-service/
-cp -r ./config/env /mnt/storage/im-worker-volume
-docker build -t im-worker .
+cp -r ./config/env /mnt/storage/im-worker-volume/config
+sudo docker build -t im-worker .
 #sudo docker compose up -d im-worker --build
